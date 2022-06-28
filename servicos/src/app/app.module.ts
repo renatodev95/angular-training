7015492a7b9747ac49c50e8d462b4a89492c1914
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-06-28 14:58:51 -0300

    Utilizando serviço em um componente com Injecao de Dependencia

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoService } from './cursos/cursos.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CursoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
