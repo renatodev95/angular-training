9405f3cbc8bd8ed10c5bb6d91b3c5dd5ff278c27
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-07-04 16:28:18 -0300

    #53: Rotas: Definindo e extraindo parâmetros de roteamento

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
 ];
--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
