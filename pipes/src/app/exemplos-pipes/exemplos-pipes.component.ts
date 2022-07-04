a6ef713f01477c2fcd72f38d26d401af8eac5f0e
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-07-04 10:47:05 -0300

    #47: Pipes: Criando um Pipe 'Impuro' e Utilizando filtros de maneira correta

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -3,30 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-exemplos-pipes',
   templateUrl: './exemplos-pipes.component.html',
-  styleUrls: ['./exemplos-pipes.component.css']
+  styleUrls: ['./exemplos-pipes.component.css'],
 })
 export class ExemplosPipesComponent implements OnInit {
-
+  
   livro: any = {
     titulo: 'Learning JavaScript Data Structures and Algorithms',
     rating: 4.54321,
     numeroPaginas: 314,
     preco: 44.99,
     dataLancamento: new Date(2016, 5, 23),
-    url: 'https://amz.onl/bjF0yWZ'
+    url: 'https://amz.onl/bjF0yWZ',
   };
 
   livros: string[] = ['Java', 'Angular 2'];
 
-  filtro: string | undefined;
+  filtro: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   addCurso(valor: string) {
     this.livros.push(valor);
     console.log(this.livros);
+  }
+
+  obterCursos() {
+    if (this.livros.length === 0 || this.filtro === undefined || this.filtro?.trim() === '') {
+      return this.livros;
+    }
+    return this.livros.filter((v) => {
+      if (v.toLowerCase().indexOf(this.filtro.toLowerCase()) != -1) {
+        return true;
+      }
+      return false;
+    });
   }
 }
