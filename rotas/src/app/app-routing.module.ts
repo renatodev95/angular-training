df700819ad9166a5fe3c18055ee6d6500b0ae57d
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-07-04 15:33:53 -0300

    #49/#50: Introducao a rotas e Configurando rotas simples

---
+++ rotas/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'cursos', component: CursosComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
