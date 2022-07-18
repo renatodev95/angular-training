8a08495419838b823457d4dc66eb380106dbb886
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-07-18 15:41:11 -0300

    #83: Forms (template driven) Refatorando (simplificando) CSS e mensagens de erro

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -3,21 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
-  styleUrls: ['./template-form.component.css']
+  styleUrls: ['./template-form.component.css'],
 })
 export class TemplateFormComponent implements OnInit {
-
+  
   usuario: any = {
     nome: null,
-    email: null
+    email: null,
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: any) {}
+
+  verificaValidTouched(campo:any) {
+    return !campo.valid && campo.touched;
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSubmit(form: any) {
-    console.log(form)
+  aplicaCssErro(campo: any) {
+    return {
+      'needs-validation': this.verificaValidTouched(campo),
+      'was-validated': this.verificaValidTouched(campo),
+    };
   }
 }
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { FormDebugComponent } from '../form-debug/form-debug.component';
 
+import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
 import { TemplateFormComponent } from './template-form.component';
 
 @NgModule({
-  declarations: [TemplateFormComponent, FormDebugComponent],
+  declarations: [
+    TemplateFormComponent,
+    FormDebugComponent,
+    CampoControlErroComponent,
+  ],
   imports: [CommonModule, FormsModule],
 })
 export class TemplateFormModule {}
