1a8b74f9acca39cdc89e879a110d55fc228e6a13
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-07-19 14:36:02 -0300

    #90: Formulários reativos: Criando um form com código Angular

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
   templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.css']
+  styleUrls: ['./data-form.component.css'],
 })
 export class DataFormComponent implements OnInit {
-
   formulario: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-  }
+    // this.formulario = new FormGroup({
+    //   nome: new FormControl(null),
+    //   email: new FormControl(null)
+    // });
 
+    this.formulario = this.formBuilder.group({
+      nome: [null],
+      email: [null],
+    });
+  }
 }
