c65aaaaf1c8fc681a29286a15362f4d926ae59e6
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-07-22 11:21:33 -0300

    #103: Formulários reativos: Combobox com Objeto (ngValue e compareWith)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -15,6 +15,7 @@ export class DataFormComponent implements OnInit {
   formulario: FormGroup;
   // estados: EstadoBr[];
   estados: Observable<EstadoBr[]>;
+  cargos: any[];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -25,6 +26,7 @@ export class DataFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.estados = this.dropdownService.getEstadosBr();
+    this.cargos = this.dropdownService.getCargos();
 
     // this.dropdownService.getEstadosBr().subscribe(dados => {
     //     this.estados = dados;
@@ -47,7 +49,9 @@ export class DataFormComponent implements OnInit {
         bairro: [null, Validators.required],
         cidade: [null, Validators.required],
         estado: [null, Validators.required]
-      })
+      }),
+
+      cargo: [null]
     });
     // [Validators.required, Validators.min(3), Validators.max(20)]
   }
@@ -135,5 +139,14 @@ export class DataFormComponent implements OnInit {
         estado: null,
       },
     });
+  }
+
+  setarCargo() {
+    const cargo = {nome: 'Dev', nivel: 'Pleno', desc: 'Dev Pl'};
+    this.formulario.get('cargo').setValue(cargo);
+  }
+
+  compararCargos(obj1, obj2) {
+    return obj1 && obj2 ? (obj1.nome === obj2.nome && obj1.nivel === obj2.nivel) : obj1 && obj2;
   }
 }
