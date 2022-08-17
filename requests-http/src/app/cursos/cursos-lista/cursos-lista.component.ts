e8b9b5b4c88e9cea3363a6db5d5e75523a72fc9a
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-08-17 14:37:06 -0300

    #124: Capturando Erros (+ Erro com async)

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Curso} from "../curso";
 import {CursosService} from "../cursos.service";
-import {Observable} from "rxjs";
+import {EMPTY, empty, Observable, Subject} from "rxjs";
+import {catchError} from "rxjs/operators";
 
 @Component({
   selector: 'app-cursos-lista',
@@ -13,7 +14,8 @@ export class CursosListaComponent implements OnInit {
 
   // cursos: Curso[];
 
-  cursos$: Observable<Curso[]>
+  cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) {
   }
@@ -22,7 +24,46 @@ export class CursosListaComponent implements OnInit {
     /*this.service.list()
       .subscribe(dados => this.cursos = dados);*/
 
-    this.cursos$ = this.service.list();
+    // this.service.list()
+    //   .subscribe({
+    //     next:(v) => {
+    //       this.cursos$ = v;
+    //     },
+    //     error:(e) => {
+    //       catchError(error => {
+    //         this.error$.next(true);
+    //         return EMPTY;
+    //       })
+    //     },
+    //     complete: () => console.log('completo')
+    //   });
+
+    this.onRefresh();
+  }
+
+  onRefresh() {
+    this.cursos$ = this.service.list().pipe(
+      // map(),
+      // tap(),
+      // switchMap(),
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return EMPTY;
+      })
+    );
+
+    // this.service.list()
+    // .pipe(
+    //   catchError(error => empty())
+    // )
+    // .subscribe(
+    //   dados => {
+    //     console.log(dados);
+    //   }
+    //   // ,error => console.error(error),
+    //   // () => console.log('Obserservable completo!')
+    // );
   }
 
 }
