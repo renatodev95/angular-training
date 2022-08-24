b55413d762b9edcb6f22395af301f90693071ed0
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-08-24 17:58:15 -0300

    #133: Popup de Confirmação genérica Bootstrap 4 (com RxJS)

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -2,10 +2,9 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {Curso} from "../curso";
 import {CursosService} from "../cursos.service";
 import {EMPTY, Observable, Subject} from "rxjs";
-import {catchError} from "rxjs/operators";
+import {catchError, switchMap, take} from "rxjs/operators";
 import {AlertModalService} from "../../shared/alert-modal.service";
 import {ActivatedRoute, Router} from "@angular/router";
-import {environment} from "../../../environments/environment";
 import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
 
 @Component({
@@ -52,7 +51,21 @@ export class CursosListaComponent implements OnInit {
 
   onDelete(curso) {
     this.cursoSelecionado = curso;
-    this.deleteModalRef = this.modalService.show(this.deleteModal, {class: 'modal-sm'});
+    // this.deleteModalRef = this.modalService.show(this.deleteModal, {class: 'modal-sm'});
+    const result$ = this.alertService.showConfirm('Confirmacao', 'Tem certeza que deseja remover esse curso?');
+    result$.asObservable()
+      .pipe(
+        take(1),
+        switchMap(result => result ? this.service.remove(this.cursoSelecionado.id) : EMPTY)
+      ).subscribe(
+        success => {
+          this.onRefresh();
+          this.alertService.showAlertSuccess('Curso removido com sucesso');
+        },
+        error => {
+          this.alertService.showAlertDanger('Erro ao remover curso. Tente novamente mais tarde.');
+        }
+    );
   }
 
   onConfirmDelete() {
--- requests-http/src/app/shared/alert-modal.service.ts
+++ requests-http/src/app/shared/alert-modal.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {AlertModalComponent} from "./alert-modal/alert-modal.component";
 import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
+import {ConfirmModalComponent} from "./confirm-modal/confirm-modal.component";
 
 export enum AlertTypes {
   DANGER = 'danger',
@@ -15,7 +16,7 @@ export class AlertModalService {
   constructor(private modalService: BsModalService) {
   }
 
-  private showAlert( message: string, type: string, dismissTimeOut?: number) {
+  private showAlert(message: string, type: string, dismissTimeOut?: number) {
     const bsModalRef: BsModalRef = this.modalService.show(AlertModalComponent);
     bsModalRef.content.type = type;
     bsModalRef.content.message = message;
@@ -30,5 +31,20 @@ export class AlertModalService {
 
   showAlertSuccess(message: string) {
     this.showAlert(message, AlertTypes.SUCCESS, 3000);
+  }
+
+  showConfirm(title: string, msg: string, okTxt?: string, cancelTxt?: string) {
+    const bsModalRef: BsModalRef = this.modalService.show(ConfirmModalComponent);
+    bsModalRef.content.title = title;
+    bsModalRef.content.msg = msg;
+
+    if (okTxt) {
+      bsModalRef.content.okTxt = okTxt;
+    }
+    if (cancelTxt) {
+      bsModalRef.content.cancelTxt = cancelTxt;
+    }
+
+    return (<ConfirmModalComponent>bsModalRef.content).confirmResult;
   }
 }
