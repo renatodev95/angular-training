be1515a457daa7500cd871eeeda1c293a173144b
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-08-31 10:25:04 -0300

    #136: Http: Upload de Arquivo: Configurando Back-end com Node.js

---
+++ requests-http/server/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const multiparty = require('connect-multiparty');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const multipartMiddleware = multiparty({ uploadDir: './uploads' });
+
+app.post('/upload', multipartMiddleware, (req, res) => {
+  const files = req.files;
+  console.log(files);
+  res.json({ message: files });
+});
+
+app.listen(8000, () => {
+  console.log('Servidor porta 8000');
+});
