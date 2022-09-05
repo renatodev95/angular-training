09d7371cb59b35c77f793c19b291812f0700d4a0
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-09-05 14:16:03 -0300

    #140: Http: Criando operador RxJS customizado

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -3,6 +3,7 @@ import {UploadFileService} from "../upload-file.service";
 import {Subscription} from "rxjs";
 import {environment} from "../../../environments/environment";
 import {HttpEvent, HttpEventType} from "@angular/common/http";
+import {filterResponse, uploadProgress} from "../../shared/rxjs-operators";
 
 @Component({
   selector: 'app-upload-file',
@@ -37,18 +38,23 @@ export class UploadFileComponent implements OnInit, OnDestroy {
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.inscricao = this.service.upload(this.files, `${environment.BASE_URL}/upload`)
-        .subscribe((event: HttpEvent<Object>) => {
-          // HttpEventType
-          console.log(event);
-          if (event.type === HttpEventType.Response) {
-            console.log('Upload concluído');
-          } else if (event.type === HttpEventType.UploadProgress) {
-            const percentDone = Math.round((event.loaded * 100) / event.total);
-            console.log('Progresso', percentDone);
-            this.progress = percentDone;
-          }
-
-        });
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        ).subscribe(response => console.log('Upload concluído'));
+        // .subscribe((event: HttpEvent<Object>) => {
+        //   // console.log(event);
+        //   if (event.type === HttpEventType.Response) {
+        //     console.log('Upload concluído');
+        //   } else if (event.type === HttpEventType.UploadProgress) {
+        //     const percentDone = Math.round((event.loaded * 100) / event.total);
+        //     // console.log('Progresso', percentDone);
+        //     this.progress = percentDone;
+        //   }
+        // });
     }
   }
 
