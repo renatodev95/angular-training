c71e398db18788d4e988c851c12426c7a07678f6
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-09-12 15:49:14 -0300

    #144: Http: Pesquisa/Busca com Programação Reativa

--- requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {map, tap} from "rxjs/operators";
+import {debounceTime, distinctUntilChanged, filter, map, switchMap, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-lib-search',
@@ -11,8 +11,9 @@ import {map, tap} from "rxjs/operators";
 })
 export class LibSearchComponent implements OnInit {
 
-  queryField = new FormControl();
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  readonly FIELDS = 'name,description,version,homepage';
+  queryField = new FormControl();
   results$: Observable<any>;
   total: number = 0;
 
@@ -20,23 +21,34 @@ export class LibSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.results$ = this.queryField.valueChanges
+      .pipe(
+        map(value => value.trim()),
+        filter(value => value.length > 1),
+        debounceTime(200),
+        distinctUntilChanged(),
+        //tap(value => console.log(value)),
+        switchMap(value => this.http.get(this.SEARCH_URL, {params: {search: value, fields: this.FIELDS}})),
+        tap((res: any) => this.total = res.total),
+        map((res: any) => res.results)
+      );
   }
 
   onSearch() {
-    const fields = 'name,description,version,homepage';
+
     let value = this.queryField.value;
     if (value && (value = value.trim()) !== '') {
 
       const params_ = {
         search: value,
-        fields: fields
+        fields: this.FIELDS
       };
 
       let params = new HttpParams();
       params = params.set('search', value);
-      params = params.set('fields', fields);
+      params = params.set('fields', this.FIELDS);
 
-      this.results$ = this.http.get(this.SEARCH_URL, { params })
+      this.results$ = this.http.get(this.SEARCH_URL, {params})
         .pipe(
           tap((res: any) => this.total = res.total),
           map((res: any) => res.results)
